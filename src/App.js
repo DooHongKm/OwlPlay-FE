0fca7b4e348b339f1a699565da440b2dff2341af
DooHongKm <rlaenghdrlae@naver.com>
2024-05-21 19:34:18 +0900

    Added layout of login success page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import Logo from './pages/Logo'
-import Header from './features/Header'
-import HeaderNotLogged from './features/HeaderNotLogged'
 import Login from './pages/Login'
+import LoginSuccess from './pages/LoginSuccess'
 
 export default function App() {
   return (
     <div className='single-page'>
-      <Logo/>
-      {/* <Header/> 레이아웃 완료 */}
-      {/* <HeaderNotLogged/> 레이아웃 완료 */}
+      {/* <Logo/> 레이아웃 완료 */}
       {/* <Login/> 레이아웃 완료 */}
+      {/* <LoginSuccess/> 레이아웃 완료 */}
     </div>
   )
 }
--- src/pages/LoginSuccess.jsx
+++ src/pages/LoginSuccess.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Owl from '../Owl.svg'
 
 export default function LoginSuccess() {
   return (
-    <div>
-      
+    <div className='loginsuccess-container'>
+      <div className='loginsuccess-box'>
+        <img src={Owl} alt='Owl'/>
+        <p>로그인에 성공하여<br/>메인 추천 페이지로 이동합니다</p>
+      </div>
     </div>
   )
 }
