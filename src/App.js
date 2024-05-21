30fa34dfa925a3c2e7af71d6720d94a6c1219acf
DooHongKm <rlaenghdrlae@naver.com>
2024-05-21 19:09:18 +0900

    Added layout of login page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Logo from './pages/Logo'
 import Header from './features/Header'
 import HeaderNotLogged from './features/HeaderNotLogged'
+import Login from './pages/Login'
 
 export default function App() {
   return (
-    <div>
-      {/* <Logo/> 완료 */}
+    <div className='single-page'>
+      <Logo/>
       {/* <Header/> 레이아웃 완료 */}
       {/* <HeaderNotLogged/> 레이아웃 완료 */}
+      {/* <Login/> 레이아웃 완료 */}
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import OwlLogo from '../OwlLogo.svg'
 
 export default function Login() {
   return (
-    <div>
-      
+    <div className='login-container'>
+      <div className='login-box'>
+        <div className='login-logo'>
+          <img src={OwlLogo} alt='OwlLogo'/>
+        </div>
+        <span/>
+        <div className='login-input'>
+          <input value="ID"></input>
+          <p>존재하지 않는 아이디입니다.</p>
+        </div>
+        <div className='login-input'>
+          <input value="PW"></input>
+          <p>잘못된 비밀번호입니다.</p>
+        </div>
+        <div className='login-button'>
+          <button type='button'>로그인</button>
+          <button type='button'>회원가입</button>
+        </div>
+      </div>
     </div>
   )
 }
