5c3382330fa5890c2830a7264caf9d3907e13145
DooHongKm <rlaenghdrlae@naver.com>
2024-05-21 11:27:08 +0900

    Added layout of header not logged

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Logo from './pages/Logo'
 import Header from './features/Header'
+import HeaderNotLogged from './features/HeaderNotLogged'
 
 export default function App() {
   return (
     <div>
-      {/* <Logo/> 테스트 통과 */}
-      {/* <Header/> 테스트 통과 */}
+      {/* <Logo/> 완료 */}
+      {/* <Header/> 레이아웃 완료 */}
+      {/* <HeaderNotLogged/> 레이아웃 완료 */}
     </div>
   )
 }
--- src/features/HeaderNotLogged.jsx
+++ src/features/HeaderNotLogged.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import OwlLogo from '../OwlLogo.svg'
 
 export default function HeaderNotLogged() {
   return (
-    <div>
-      
+    <div className='header-container'>
+      <img src={OwlLogo} alt='OwlLogo'/>
+      <button type='button'>Home</button>
+      <button type='button'>Movie</button>
+      <button type='button'>Drama</button>
+      <button type='button'>Entertainment</button>
+      <button type='button'>Animation</button>
+      <div className='empty-space'></div>
+      <div className='search-box'>
+        <input></input>
+        <button type='button'>⌕</button>
+      </div>
+      <button type='button'>회원가입</button>
     </div>
   )
 }
