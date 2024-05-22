627ef1c047993df8e139c4477d2cc69f80152769
DooHongKm <rlaenghdrlae@naver.com>
2024-05-22 11:31:48 +0900

    Added layout of signup success page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Logo from './pages/Logo/Logo'
 import Login from './pages/Login/Login'
 import LoginSuccess from './pages/LoginSuccess/LoginSuccess'
 import Signup from './pages/Signup/Signup'
+import SignupSuccess from './pages/SignupSuccess/SignupSuccess'
+import Test from './pages/Test/Test'
 
 export default function App() {
   return (
@@ -10,7 +12,9 @@ export default function App() {
       {/* <Logo/> 레이아웃 완료 */}
       {/* <Login/> 레이아웃 완료 */}
       {/* <LoginSuccess/> 레이아웃 완료 */}
-      <Signup/>
+      {/* <Signup/> 레이아웃 완료 */}
+      {/* <SignupSuccess/> 레이아웃 완료 */}
+      <Test/>
     </div>
   )
 }
--- src/pages/SignupSuccess/SignupSuccess.jsx
+++ src/pages/SignupSuccess/SignupSuccess.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './SignupSuccess.css'
+import Owl from '../../Owl.svg'
 
 export default function SignupSuccess() {
   return (
-    <div>
-      
+    <div className='signupsuccess-container'>
+      <div className='signupsuccess-box'>
+        <img src={Owl} alt='Owl'/>
+        <p>회원가입에 성공하여<br/>성향 조사 페이지로 이동합니다</p>
+      </div>
     </div>
   )
 }
