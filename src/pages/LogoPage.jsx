6323a255934724c540dab5575b248775436e2529
DooHongKm <rlaenghdrlae@naver.com>
2024-08-02 14:01:29 +0900

    Modify logo page

--- src/pages/LogoPage.jsx
+++ src/pages/LogoPage.jsx
@@ -1,26 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import './Logo.css'
-import {ReactComponent as OwlLogo} from '../../images/OwlLogo.svg'
-import Login from '../Login/Login';
+// import react
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function LogoPage({ setUserId }) {
+// import assets
+import { ReactComponent as OwlLogo } from '../assets/images/OwlLogo.svg';
 
-  // 3초 후 Login Page로 이동
-  const [showLogo, setShowLogo] = useState(true);
+// import style sheets
+import '../styles/pages/LogoPage.css';
+
+// logo page component
+const LogoPage = () => {
+
+  // navigate to login page (3s)
+  const navigate = useNavigate();
   useEffect(() => {
     const timer = setTimeout(() => {
-      setShowLogo(false);
+      navigate('/login');
     }, 3000);
     return () => clearTimeout(timer);
   }, []);
 
+  // return
   return (
-    <div className='switch-container'>
-      {showLogo ? 
-        <div className='logo-container'>
-          <OwlLogo/>
-        </div>
-      : <Login setUserId={setUserId}/>}
+    <div className='logo-container'>
+      <OwlLogo className='logo-image'/>
     </div>
   )
 }
+
+// export
+export default LogoPage;
